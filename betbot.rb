4473cb93e61083eed1f9ee8c3b6f9cb44c2e94d5
Andrei Shindyapin <andrei.shindyapin@gmail.com>
2010-01-02 21:48:47 -0500

    forgot that I was tracking my bot with git; this is an update
    Signed-off-by: Andrei Shindyapin <andrei.shindyapin@gmail.com>

--- betbot.rb
+++ betbot.rb
@@ -3,17 +3,27 @@ require 'sinatra'
 require 'json'
 require 'rest_client'
 
-wtt_url = 'http://localhost:3000'
+#change the following to http://www.winthetrophy.com/apiv1 when available
+wtt_url = 'http://localhost:3000/apiv1'
 api_key = '2bfd15f70be65116b6c0f29a701a97a0632adeb5'
 
-#Get # of chips for the current betweek; if zero, you're done until the next time you check.
+#Get # of chips for the current betweek.
+#If none, you're done until the next time you check.
 get '/' do
-  t3st = RestClient.get "#{wtt_url}/apiv1/leagues.json?api_key=#{api_key}"
-  "#{JSON.parse(t3st)}"
+  bet_weeks = RestClient.get "#{wtt_url}/bet_weeks.json?api_key=#{api_key}"
+  bet_weeks = JSON.parse(bet_weeks)  
+  chips = bet_weeks[0]['bet_week']['chips']
+
+  "You have #{chips} chips.<br /><br />"
+  if chips == 0
+    "Try again later!"
+  else
+    #Do stuff here...
+  end
 end
 
 #Get all the games in the next 24 hours.
 #For each game, determine the highest payout; record the points for it.
-#Add all the points together and divide by the available chips = chips_per_point.
+#Sum all the points and divide by the available chips = chips_per_point.
 #For each game, bet points*chips_per_point on the highest payout.
 #Check again in an hour to see if you have won anything.
