4ac2947b559cb9e9b7255335b2a469250a1be7bd
Andrei Shindyapin <andrei.shindyapin@gmail.com>
2010-01-09 10:50:42 -0500

    general cleanup of stuff

--- betbot.rb
+++ betbot.rb
@@ -5,21 +5,28 @@ require 'rest_client'
 require 'haml'
 
 #change the following to http://www.winthetrophy.com/apiv1 when available
-wtt_url = 'http://localhost:3000/apiv1'
-api_key = '2bfd15f70be65116b6c0f29a701a97a0632adeb5'
+wtt_url   = 'http://localhost:3000/apiv1'
+api_key   = 'b1d8338ee7ad9c257e4b91fbf23777b1d0979788'
+bet_weeks = "#{wtt_url}/bet_weeks.json?api_key=#{api_key}"
+events    = "#{wtt_url}/events.json?api_key=#{api_key}"
 
-#Get # of chips for the current betweek.
-#If none, you're done until the next time you check.
 get '/' do
-  bet_weeks = RestClient.get "#{wtt_url}/bet_weeks.json?api_key=#{api_key}"
-  @bet_weeks = JSON.parse(bet_weeks)  
-  @chips = @bet_weeks[0]['bet_week']['chips']
+  #Get # of chips for the current betweek.
+  #If none, you're done until the next time you check.
+  @bet_weeks = JSON.parse(RestClient.get(bet_weeks))
+  @chips = @bet_weeks[0]['chips']
+
+  unless @chips == 0
+    #Get all the games in the next 24 hours.
+    @events = JSON.parse(RestClient.get(events))
+
+    #For each game, determine the highest payout; record the points for it.
+#    points = price.abs/100 if price > 0
+#    points = 100/price.abs if price < 0
+  end
 
   haml :index
 end
 
-#Get all the games in the next 24 hours.
-#For each game, determine the highest payout; record the points for it.
 #Sum all the points and divide by the available chips = chips_per_point.
 #For each game, bet points*chips_per_point on the highest payout.
-#Check again in an hour to see if you have won anything.
