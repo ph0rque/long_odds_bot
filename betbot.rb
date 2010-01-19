d9da27ba607db051e448cfa291fe4d9aa19c0446
Andrei Shindyapin <andrei.shindyapin@gmail.com>
2010-01-18 21:31:31 -0500

    further dev of the betbot... the logic is pretty much done. Now to make a bet...

--- betbot.rb
+++ betbot.rb
@@ -19,25 +19,38 @@ get '/' do
   unless @chips == 0
     #Get all the games in the next 24 hours.
     @events = JSON.parse(RestClient.get(events))
-    #For each game, determine the highest payout; record the points for it.
-#    @events.each do |game|
-#      over_price  = game['overunder']['over_price']
-#      under_price = game['overunder']['under_price']
-#      home_price  = game['moneyline']['home_line']
-#      away_price  = game['moneyline']['away_line']
-#      home_spread = game['spread']['home_price']
-#      away_spread = game['spread']['away_price']
 
-#      game[over_payout]  = over_price  < 0 ? -100/over_price  : over_price /100
-#      game[under_payout] = under_price < 0 ? -100/under_price : under_price/100
-#      game[home_payout]  = home_price  < 0 ? -100/home_price  : home_price /100
-#      game[away_payout]  = away_price  < 0 ? -100/away_price  : away_price /100
-#      game[home_spread]  = home_spread < 0 ? -100/home_spread : home_spread/100
-#      game[away_spread]  = away_spread < 0 ? -100/away_spread : away_spread/100
+    #For each game, determine the max payout; record the points for it.
+    @events.each do |game|
+      if game['overunder']
+        over_price  = game['overunder']['over_price']  || 0
+        under_price = game['overunder']['under_price'] || 0
+      else over_price = under_price = 0;  end
 
-#    end
-#    #Sum all the points and divide by the available chips = chips_per_point.
-#    #For each game, bet points*chips_per_point on the highest payout.
+      if game['moneyline']  
+        home_price  = game['moneyline']['home_line']   || 0
+        away_price  = game['moneyline']['away_line']   || 0
+      else home_price = away_price = 0;   end
+
+      if game['spread']
+        home_spread = game['spread']['home_price']     || 0
+        away_spread = game['spread']['away_price']     || 0
+      else home_spread = away_spread = 0; end
+
+      game['stats'] = {}
+      game['stats']['over_payout']  = over_price  < 0 ? -100/over_price  : over_price /100
+      game['stats']['under_payout'] = under_price < 0 ? -100/under_price : under_price/100
+      game['stats']['home_payout']  = home_price  < 0 ? -100/home_price  : home_price /100
+      game['stats']['away_payout']  = away_price  < 0 ? -100/away_price  : away_price /100
+      game['stats']['home_spread']  = home_spread < 0 ? -100/home_spread : home_spread/100
+      game['stats']['away_spread']  = away_spread < 0 ? -100/away_spread : away_spread/100
+
+      game['max_amount'] = game['stats'].values.max
+      game['max_payout'] = game['stats'].key(game['max_amount'])
+    end
+
+    #Sum all the points and divide by the available chips = chips_per_point.
+    #For each game, bet points*chips_per_point on the highest payout.
   end
 
   haml :index
