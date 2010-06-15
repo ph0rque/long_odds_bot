722c0a325628d51bf3aeecdf0690dbfda462c7c4
Andrei Shindyapin <andrei.shindyapin@gmail.com>
2010-06-14 23:28:51 -0400

    see if this works

--- betbot_config.rb
+++ betbot_config.rb
@@ -1,11 +1,12 @@
 WTT_URL   = 'http://www.winthetrophy.com/apiv1'
-
-# Put your API Key in a file called ./api.key
-API_KEY = File.read(File.join(File.dirname(__FILE__), 'api.key'))
-
 BET_WEEKS = "#{WTT_URL}/bet_weeks.json?api_key=#{API_KEY}"
 EVENTS    = "#{WTT_URL}/events.json?api_key=#{API_KEY}"
 BET_URL   = "#{WTT_URL}/bets"
+STATUS    = 'ok' # should be ok unless something is broken
 
-STATUS    = 'ok' #should be ok unless something is broken
+# when running locally, the API key will be taken from the local_api_key file,
+# which is gitignored.
+# before uploading to e.g. heroku, run the following:
+  # heroku config:add API_KEY=my_api_key
+API_KEY   = ENV['API_KEY'] or File.read(File.join(File.dirname(__FILE__), 'local_api_key'))
 
